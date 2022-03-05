0bd4f80ffc96feebd4786cccfb599d2279790161
Abdullah-alnoumas <70814254+Abdullah-alnoumas@users.noreply.github.com>
2022-03-05 11:42:46 +0300

    pictures

--- ios-cw-3/ios-cw-3/ContentView.swift
+++ ios-cw-3/ios-cw-3/ContentView.swift
@@ -9,8 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack { Text("welcome, to iOS!")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.red)
+                .padding()
+            
+            Image("pic1").resizable().frame(width: 175 , height: 175, alignment: .center)
+                .scaledToFit()
+            
+            Text("Ironman ")
+                .font(.headline)
+            
+            Image("pic2").resizable().frame(width: 175 , height: 175, alignment: .center)
+                .scaledToFit()
+            Text("spiderman ")
+                .font(.headline)
+
+            Image("pic 3").resizable().frame(width: 175 , height: 175, alignment: .center)
+                .scaledToFit()
+            
+            
+            Text("Batman ")
+                .font(.headline)
+            
+// I did not include HStack because this is more organized
+       
+        }
+
+        
     }
 }
 
